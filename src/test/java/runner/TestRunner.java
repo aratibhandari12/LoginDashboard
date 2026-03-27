ab0438eded5f157b0c2fe09e20787cd79e945c0f
aratibhandari12 <aratibhandari12@gmail.com>
2026-03-26 22:33:45 -0500

    Initial commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/index.html",
+                "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml"
+        },
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
